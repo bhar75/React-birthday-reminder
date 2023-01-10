adef3dc1af8a4d5d535346419c6945e273f0a8e3
bhar75 <bharathi_n@msn.com>
2023-01-10 11:51:11 +0530

    simple useState app

---
+++ src/List.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+
+const List = (props) => {
+  const { people } = props;
+  return (
+    <>
+      {people.map((person) => {
+        const { id, name, age, image } = person;
+        return (
+          <article className="person" key={id}>
+            <img src={image} alt={name} />
+            <div>
+              <h4>{name}</h4>
+              <p>{age}</p>
+            </div>
+          </article>
+        );
+      })}
+    </>
+  );
+};
+
+export default List;
